2521990be3fb7be9c60c01a76f3b463b96b94af8
George Armhold <armhold@gmail.com>
2016-04-01 16:30:29 -0400

    paranoia testing of RandInt()

---
+++ util_test.go
@@ -0,0 +1,15 @@
+package polygen
+
+import (
+	"testing"
+)
+
+func TestRandomInt(t *testing.T) {
+	for i := 0 ; i < 1000; i++ {
+		d := RandomInt(1, 4)
+
+		if d < 1 || d > 3 {
+			t.Fatalf("expected d to be in [1..3], was: %d", d)
+		}
+	}
+}
