734b741b02ee3889b802e727d44fe3f73e951357
George Armhold <armhold@gmail.com>
2016-04-19 10:47:06 -0400

    make "out of box" experience easier for users:
    1. default to source image: images/mona_lisa.jpg
    2. automatically derive checkpoint file name
    3. tie checkpoint file name to source file + polyCount
    4. detect polyCount arg vs checkpoint file mismatch

--- cmd/shiny/shiny.go
+++ cmd/shiny/shiny.go
@@ -24,7 +24,7 @@ var (
 	polyCount  int
 	srcImgFile string
 	dstImgFile string
-	cpFile     string
+	cpArg string
 )
 
 func init() {
@@ -32,7 +32,7 @@ func init() {
 	flag.IntVar(&polyCount, "poly", 50, "the number of polygons")
 	flag.StringVar(&srcImgFile, "source", "images/mona_lisa.jpg", "the source input image file")
 	flag.StringVar(&dstImgFile, "dest", "output.png", "the output image file")
-	flag.StringVar(&cpFile, "cp", "checkpoint.tmp", "checkpoint file")
+	flag.StringVar(&cpArg, "cp", "", "checkpoint file")
 
 	flag.Parse()
 
@@ -47,7 +47,9 @@ func init() {
 func main() {
 	refImg := polygen.MustReadImage(srcImgFile)
 
-	evolver, err := polygen.NewEvolver(refImg, dstImgFile, cpFile)
+	cp := polygen.DeriveCheckpointFile(srcImgFile, cpArg, polyCount)
+
+	evolver, err := polygen.NewEvolver(refImg, dstImgFile, cp)
 	if err != nil {
 		log.Fatal(err)
 	}
